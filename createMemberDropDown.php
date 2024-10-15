085589434a11035b8616d8af69a324cd9a37b461
JackONeill03 <B00155573@mytudublin.ie>
2024-10-15 14:44:51 +0100

    Added Part 3

---
+++ createMemberDropDown.php
@@ -0,0 +1,30 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tennisclub";
+$port = 3306;
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * from member";
+$result = mysqli_query($conn, $sql);
+
+echo "<SELECT name='memberID'>";
+
+while($row = mysqli_fetch_assoc($result)) {
+    $id=$row['id'];
+    $fn=$row['firstname'];
+    $sn=$row['surname'];
+    echo "<OPTION value='$id'>$fn $sn</OPTION>";
+}
+echo "</SELECT>";
+
+mysqli_close($conn); 
+?>
